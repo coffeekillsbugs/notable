21e2572cf798a22e903892063498afb921de3b04
Nihar Lekhade <54531613+mistMoriarty@users.noreply.github.com>
2019-09-05 17:38:40 +0530

    Major
    Organised code

---
+++ lib/custom_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Color smokyBlack = Color(0xFF090C08);
+Color outerSpace = Color(0xFF424C55);
+Color carribeanGreen = Color(0xFF09BC8A);
+Color isabelline = Color(0xFFF5EDF0);
+Color charlestonGreen = Color(0xFF2E2C2F);
+Color gunMetal = Color(0xFF3F4045);
+Color harvardCrimson = Color(0xFFC20114);
