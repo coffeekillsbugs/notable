a78ef8fcbba3378ad73945f180a9221e93ca7c96
Nihar Lekhade <arthurexcalibur9@protonmail.com>
2019-11-30 13:28:05 +0530

    Improved UI/UX

--- lib/Screens/landingScreen.dart
+++ lib/Screens/landingScreen.dart
@@ -9,119 +9,129 @@ class LandingPage extends StatefulWidget {
 class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      /*----AppBar---*/
-      appBar: AppBar(
-        /*---Icon Button---*/
-        leading: IconButton(
-              onPressed:() {
-                
-              },
-              icon: Icon(Icons.sort, color: carribeanGreen),
-            ),
-        /*---Notable---*/
-        title: Text(
-              'NOTABLE',
-              style: TextStyle(
-                color: carribeanGreen,
-                fontFamily: 'Righteous',
-                fontSize: 25.0,
-                //fontWeight: FontWeight.w600,
+    final screenSize = MediaQuery.of(context).size;
+    print(screenSize.width);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            Scaffold(
+              resizeToAvoidBottomInset: false,
+              appBar: AppBar(
+                backgroundColor: smokyBlack,
+                leading: InkWell(
+                  child: Icon(Icons.sort, color: carribeanGreen),
+                ),
+                title: Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    'NOTABLE',
+                    style: TextStyle(
+                      color: carribeanGreen,
+                      fontFamily: 'Righteous',
+                      fontSize: 25.0,
+                      //fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
               ),
+              backgroundColor: smokyBlack,
+              /*---Body---*/
+              body: GridView(
+                physics: BouncingScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, 
+                  childAspectRatio: 0.75,
+                  crossAxisSpacing: 0.0,
+                  mainAxisSpacing: 0.0,),
+                children: <Widget>[
+                  NoteLayout(screenSize: screenSize),
+                  NoteLayout(screenSize: screenSize),
+                  NoteLayout(screenSize: screenSize),
+                  NoteLayout(screenSize: screenSize),
+                  NoteLayout(screenSize: screenSize),
+                  NoteLayout(screenSize: screenSize),
+                  NoteLayout(screenSize: screenSize),
+                  NoteLayout(screenSize: screenSize),
+                ],
+              ),
+              floatingActionButton: AddNote(),
+              floatingActionButtonLocation:
+                  FloatingActionButtonLocation.centerDocked,
             ),
-          centerTitle: true,
-      ),
-      /*---Body---*/
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 1,
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: NoteCard(),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: NoteCard(),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: NoteCard(),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: NoteCard(),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: NoteCard(),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: NoteCard(),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: NoteCard(),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: NoteCard(),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: NoteCard(),
-                ),
-              ],
-            ),
-          ),
-        ],
+            //CustomAppBar(screenSize: screenSize)
+          ],
+        ),
       ),
     );
   }
 }
 
-class NoteCard extends StatelessWidget {
-  
 
+class AddNote extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(20.0),
-            topRight: Radius.circular(20.0),
-            bottomRight: Radius.circular(20.0),
-            bottomLeft: Radius.circular(20.0),
-      ),
-          child: Container(
-        margin: EdgeInsets.all(4.0),
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 130.0),
+      child: Container(
+        height: 50.0,
+        alignment: Alignment.center,
         decoration: BoxDecoration(
-          color: outerSpace,
-          border: Border(
-            bottom: BorderSide(
-              width: 3.0,
-              //style: BorderStyle.solid,
-              color: carribeanGreen,
-            ),
+          borderRadius: BorderRadius.circular(10.0),
+          color: carribeanGreen,
+        ),
+        child: InkWell(
+          child: Icon(
+            Icons.add,
+            color: smokyBlack,
+            size: 35.0,
           ),
         ),
       ),
     );
   }
 }
+
+class NoteLayout extends StatelessWidget {
+  final screenSize, variableHeight;
+
+  NoteLayout({this.screenSize, this.variableHeight});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 1.0),
+      child: Container(
+       // height: 200.0,
+        //width: screenSize.width * 0.45,
+        margin: EdgeInsets.all(4.0),
+        decoration: BoxDecoration(
+            color: outerSpace,
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: [
+              BoxShadow(
+                color: carribeanGreen,
+                offset: Offset(0, 3),
+                blurRadius: 1.0,
+              ),
+            ],
+            ),
+      ),
+    );
+  }
+}
+
+/*
+CustomAppBar(
+            backgroundColor: smokyBlack,
+            /*---Icon Button---*/
+            leading: IconButton(
+              onPressed: () {},
+              icon: ,
+            ),
+            /*---Notable---*/
+            title: 
+            centerTitle: true,
+          ),
+  */
